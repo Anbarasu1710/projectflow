75cadda6cf0e6639547eeceebde258aad53ad1f0
GameChangerss <49678106+GameChangerss@users.noreply.github.com>
2025-08-20 16:20:59 +0530

    Fix all incorrect Radix UI imports

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import * as SwitchPrimitive from "@radix-ui/react-avatar";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
 
 import { cn } from "./utils";
 
--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import * as ToggleGroupPrimitive from "@radix-ui/react-avatar";
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { type VariantProps } from "class-variance-authority";
 
 import { cn } from "./utils";
